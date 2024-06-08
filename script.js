7673a8344fcaf06ac2a09a91a4c890d664e56f9d
Zach <64771834+amburnzachary@users.noreply.github.com>
2024-06-08 13:19:22 -0500

    Set button to prompt for the size of the new grid

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const container = document.querySelector("#container");
+const btn = document.querySelector("#btn");
 
 for (let i = 0; i < 256; i++) {
     const square = document.createElement("div");
@@ -10,4 +11,8 @@ container.addEventListener("mouseover", (event) => {
     let target = event.target;
     if (target != container)
         target.classList.add("hovered");
-})
+});
+
+btn.addEventListener("click", () => {
+    let size = prompt("Enter the size of the new grid:");
+});
