d858c84ad00a273208c55cf31a2dc7fe07627183
Zach <64771834+amburnzachary@users.noreply.github.com>
2024-06-09 12:21:57 -0500

    Make the opacity of the squares increment
    This creates the effect of shading, and the squares get completely
    black after 10 mouse overs.

--- script.js
+++ script.js
@@ -5,8 +5,11 @@ generateGrid(16);
 
 container.addEventListener("mouseover", (event) => {
     let target = event.target;
-    if (target != container)
+    if (target != container) {
         target.classList.add("hovered");
+        if (parseFloat(target.style.opacity) < 1)
+            target.style.opacity = parseFloat(target.style.opacity) + .1;
+    }
 });
 
 btn.addEventListener("click", () => {
@@ -23,6 +26,7 @@ function generateGrid (size) {
     for (let i = 0; i < (size * size); i++) {
         const square = document.createElement("div");
         square.classList.add("square");
+        square.style.opacity = 0;
         let boxSize = 800 / size;
         square.style.width = boxSize + "px";
         square.style.height = boxSize + "px";
